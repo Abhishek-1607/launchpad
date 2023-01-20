927b97b4747705c3f670201c9348d015ae20be35
Abhishek kumar <98520990+Abhishek-1607@users.noreply.github.com>
2023-01-20 19:07:05 +0530

    for the SELECT DATABASE TASK IN ASK 2

---
+++ task2Qc.php
@@ -0,0 +1,43 @@
+<?php
+interface er{
+    public static function reInr();
+}
+interface nr{
+    public static function getDbConn();
+}
+class pll implements er,nr{
+    private static $inr=NULL;
+    private $dbconn;
+    private function __construct(){
+        echo "Database connected <br>";
+    }
+    public static function reInr(){
+        if(self::$inr==NULL){
+            self::$inr = new Static();
+        }
+        else{
+            echo "already connected <br>";
+        }
+        return self::$inr;
+    }
+    public static function getDbConn(){
+        try{
+            $db=self::$inr;
+            $db->dbconn=new mysqli('localhost','hestabit','hestabit','info');  
+            return $db->dbconn;
+        }
+        catch(Exception $e){
+                echo "error".$e->getMessage();
+        }
+    }
+}
+$obj1=pll::reInr();
+$obj2=pll::getDbConn();
+$sql="DELETE * FROM information where id=4";
+$sql="select * from information";
+$result=$obj2->query($sql);
+$response=$result->fetch_all();
+echo "<pre>";
+print_r($response);
+
+?>
